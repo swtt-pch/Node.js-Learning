333ccbaece566fd746202bb5d4c3d6c5a4a3d92a
Igor Gomes <igor.gsouza@bandtec.com.br>
2022-04-01 21:29:22 -0300

    Nodefarm project upload

---
+++ 1-node-farm/index.js
@@ -0,0 +1,84 @@
+const fs = require("fs"); //requiring the file system module... that able us to read files
+const http = require("http");
+const url = require("url");
+const replaceTemplate = require("./module/replaceTemplate");
+
+//-------------------------------------FILES---------------------------------------//
+/*
+    //Blocking, synchronous way
+    const textIn = fs.readFileSync("./txt/input.txt", "utf-8"); // get the content of a file and returns it
+    console.log(textIn);
+    const textOut = `this is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+    fs.writeFileSync("./txt/output.txt", textOut); // write a content in a new file that you specifies in the first parameters...
+    console.log("File written!");
+*/
+
+/*
+    //Blocking, synchronous way (and an exemple of Callback Hell*)
+    fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+    // error, usually, is the first one parameter
+    fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+        console.log(data2);
+        fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
+        console.log(data3);
+        fs.writeFile(`./txt/final.txt`, `${data2}\n${data3}`, "utf-8", (err) => {
+            if (err) throw err;
+            console.log("your file has been written...");
+        });
+        });
+    });
+    });
+    console.log("Reading File...");
+*/
+
+//-------------------------------------SERVER---------------------------------------//
+const templateOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+); // read the html file and saves on a variable
+const templateCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+); // read the html file and saves on a variable
+const templateProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+); // read the html file and saves on a variable
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8"); // read the json data file and save it on a variable
+const dataObj = JSON.parse(data); // transforms the data variable into an object
+
+const server = http.createServer((req, res) => {
+  //Overview page
+  const { query, pathname } = url.parse(req.url, true); // catchs query and pathname in the req.url object
+
+  if (pathname === "/" || pathname === "/overview") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(templateCard, el))
+      .join("");
+    const output = templateOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+    res.end(output);
+    //product page
+  } else if (pathname === `/product`) {
+    res.writeHead(200, { "Content-type": "text/html" });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(templateProduct, product);
+    res.end(output);
+    //api page
+  } else if (pathname === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
+  } else {
+    //not found page
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "Hello-word",
+    });
+    res.end("<h1>This page doesn't exists</h1>");
+  }
+}); //create server that hold a request and a response
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to requests on port 8000");
+});
---
+++ 1-node-farm/module/replaceTemplate.js
@@ -0,0 +1,14 @@
+module.exports = (temp, product) => {
+  // Anonimous function
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  return output;
+}; // This function catch everysingle {%WORD%} that we mark on html file and replace with an object that we pass as an argument
